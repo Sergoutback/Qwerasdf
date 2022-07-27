b3ce49ade1c36eae15d699716d6cd159c46ff2a1
sergoutback <webart2013@gmail.com>
2022-07-27 21:59:37 +0300

    27072022_2159
    Try to add character

--- Assets/Scripts/AnimationsStart.cs
+++ Assets/Scripts/AnimationsStart.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
+//using DG.Tweening;
 public class AnimationsStart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject rectangle;
+
+    //[SerializeField] private string buttonValue;
+
+    [SerializeField] private Animation animationBig;
+
+    public void PressButtonY()
     {
-        
+        if (Input.GetKey("Y"))
+        {
+            animationBig = GetComponent<Animation>();
+
+            animationBig.Play("BigRectangleAnimation");
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
