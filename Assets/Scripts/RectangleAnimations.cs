1bf5f1eda2706702fcb83e6ca5f1f459453483b7
sergoutback <webart2013@gmail.com>
2022-07-28 01:31:32 +0300

    28072022_0131
    Add Q for reset

--- Assets/Scripts/RectangleAnimations.cs
+++ Assets/Scripts/RectangleAnimations.cs
@@ -15,48 +15,71 @@ public class RectangleAnimations: MonoBehaviour
 
     void Update()
     {
+        ButtonDown();
+    }
+
+    private void ButtonDown()
+    {
         if (Input.GetKeyDown(KeyCode.Y))
         {
+            Reset();
+
             animator.SetBool(name: "ButtonY", value: true);
         }
 
-        if (Input.GetKeyUp(KeyCode.Y))
-        {
-            animator.SetBool(name: "ButtonY", value: false);
-        }
+        //if (Input.GetKeyUp(KeyCode.Y))
+        //{
+        //    animator.SetBool(name: "ButtonY", value: false);
+        //}
 
 
         if (Input.GetKeyDown(KeyCode.B))
         {
+            Reset();
+
             animator.SetBool(name: "ButtonB", value: true);
         }
 
-        if (Input.GetKeyUp(KeyCode.B))
-        {
-            animator.SetBool(name: "ButtonB", value: false);
-        }
+        //if (Input.GetKeyUp(KeyCode.B))
+        //{
+        //    animator.SetBool(name: "ButtonB", value: false);
+        //}
 
         if (Input.GetKeyDown(KeyCode.R))
         {
+            Reset();
+
             animator.SetBool(name: "ButtonR", value: true);
         }
 
-        if (Input.GetKeyUp(KeyCode.R))
-        {
-            animator.SetBool(name: "ButtonR", value: false);
-        }
+        //if (Input.GetKeyUp(KeyCode.R))
+        //{
+        //    animator.SetBool(name: "ButtonR", value: false);
+        //}
 
         if (Input.GetKeyDown(KeyCode.G))
         {
+            Reset();
+
             animator.SetBool(name: "ButtonG", value: true);
         }
 
-        if (Input.GetKeyUp(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            animator.SetBool(name: "ButtonG", value: false);
+            Reset();
         }
 
     }
 
+    private void Reset()
+    {
+        animator.SetBool(name: "ButtonB", value: false);
 
+        animator.SetBool(name: "ButtonY", value: false);
+
+        animator.SetBool(name: "ButtonR", value: false);
+
+        animator.SetBool(name: "ButtonG", value: false);
+
+    }
 }
