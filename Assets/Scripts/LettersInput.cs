00d8bc6ffa3dba45f9a513b682b9f8a652f79f75
sergoutback <webart2013@gmail.com>
2022-07-27 11:22:58 +0300

    27072022_1121
    Add QWERASDF

--- Assets/Scripts/LettersInput.cs
+++ Assets/Scripts/LettersInput.cs
@@ -6,13 +6,6 @@ using DG.Tweening;
 
 public class LettersInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         
